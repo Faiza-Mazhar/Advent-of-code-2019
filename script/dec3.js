a16ed46b3318af59f80a25d95f9c6de7846f5443
Faiza Mazhar <faiza.mazhar@bbc.co.uk>
2019-12-06 15:52:18 +0000

    Day 3 and 4 are completed

--- script/dec3.js
+++ script/dec3.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 
 
 let wires = fs.readFileSync('../input/dec3', 'utf8').split('\n');
-wires[0] = 'R8,U5,L5,D3';
-wires[1] = 'U7,R6,D4,L4';
+// wires[0] = 'R8,U5,L5,D3';
+// wires[1] = 'U7,R6,D4,L4';
 
-  wires[0] = 'R75,D30,R83,U83,L12,D49,R71,U7,L72';
-  wires[1] = 'U62,R66,U55,R34,D71,R55,D58,R83';
+//   wires[0] = 'R75,D30,R83,U83,L12,D49,R71,U7,L72';
+//   wires[1] = 'U62,R66,U55,R34,D71,R55,D58,R83';
+
+// wires[0] = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51';
+// wires[1] = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
 
 let wirePath1 = wires[0].split(',');
 let wirePath2 = wires[1].split(',')
@@ -14,19 +17,27 @@ let wirePath2 = wires[1].split(',')
 let direction = {
     x: 0,
     y: 0,
-    step: 0,
     toString() {
-        return `${this.x}:${this.y}:${this.step}`;
+        return `${this.x}:${this.y}`;
     }
 }
 
 let directionSetWire1 = new Set();
 let directionSetWire2 = new Set();
+
+let stepTakenForWire1 = [];
+let stepTakenForWire2 =[];
+
 let crossedPosition = [];
 
-
-function mapWires(wire, set, track){
-
+const addInformation = function(set, direction, stepsTaken, steps){
+    if(!set.has(direction.toString())){
+        set.add(direction.toString());
+        stepsTaken.push(steps)
+    }
+}
+function mapWires(wire, set, stepTaken){
+    let steps = 0;
     wire.map((item ) => {
         let operation = item.substr(0,1);
         let position = parseInt(item.substr(1, item.length))
@@ -35,40 +46,35 @@ function mapWires(wire, set, track){
             case 'R':
                 while(position > 0){
                     direction.x ++;
-                    if(track)
-                        direction.step++;
                     position --;
-                    set.add(direction.toString());
+                    steps++;
+                    addInformation(set, direction, stepTaken, steps)
                 }
             break;
             case 'L':
                     while(position > 0){
                         direction.x --;
-                        if(track)
-                            direction.step++;
-                       
                         position --;
-                        set.add(direction.toString());
+                        steps++;
+                        addInformation(set, direction, stepTaken, steps)
                     }
                 
                 break;
             case 'U':
                     while(position > 0){
                         direction.y ++;
-                        if(track)
-                            direction.step++;
                         position --;
-                        set.add(direction.toString());
+                        steps++;
+                        addInformation(set, direction, stepTaken, steps)
                     }
                 break;
 
             case 'D':
                     while(position > 0){
                         direction.y --;
-                        if(track)
-                            direction.step++;
                         position --;
-                        set.add(direction.toString());
+                        steps++;
+                        addInformation(set, direction, stepTaken, steps)
                     }
                 break;
         }
@@ -78,10 +84,10 @@ function mapWires(wire, set, track){
 const resetDirection = function(direction){
     direction.x = 0;
     direction.y = 0;
-    direction.step = 0;
+    steps = 0;
 }
 
-const findDistance = function(){
+const findClosestDistance = function(){
     let distanceArray = [];
     crossedPosition.map((position) => {
         let values = position.split(':');
@@ -90,43 +96,55 @@ const findDistance = function(){
     } );
 
     distanceArray.sort(function(a, b){return a - b});
+    return distanceArray[0];
+}
 
-    console.log(crossedPosition)
-     return distanceArray[0];
+const fewStepTakenDistance = function(crossedPosition){
+    let index1, index2;
+    let currentStep , newStep;
+    let wirePathArray1 = [...directionSetWire1]
+    let wirePathArray2 = [...directionSetWire2]
+    crossedPosition.map((item) => {
+        index1 = wirePathArray1.indexOf(item)
+        index2 = wirePathArray2.indexOf(item)
+        // console.log(item, stepTakenForWire1[index1], stepTakenForWire1[index2])
+        newStep = stepTakenForWire1[index1] + stepTakenForWire2[index2]
+        // console.log(newStep)
+        if(!currentStep)
+            currentStep = newStep
+        if(newStep < currentStep)
+            currentStep = newStep;
+    }
+    )
+    return currentStep;
+
 }
 
 const findIntersectionPoints = function(set1, set2){
+    let crossedPosition = []
     for(let item of set2)
         if(set1.has(item))
             crossedPosition.push(item);
+    return crossedPosition;
 }
 
-const findShortestStepIntersectionPoint = function(set1, set2){
+
+const setUpWires = function(){
     resetDirection(direction);
-    mapWires(wirePath1, directionSetWire1, true);
+    mapWires(wirePath1, directionSetWire1, stepTakenForWire1);
 
     resetDirection(direction);
-    mapWires(wirePath2, directionSetWire2, true);
+    mapWires(wirePath2, directionSetWire2, stepTakenForWire2);
 
+    crossedPosition = findIntersectionPoints(directionSetWire1, directionSetWire2);
     
-
-    console.log(directionSetWire1, directionSetWire2)
 }
 
-const findDistanceOfClosestIntersectionPoint = function(){
-    resetDirection(direction);
-    mapWires(wirePath1, directionSetWire1, false);
+setUpWires();
+console.log('Distance: ' ,   findClosestDistance(crossedPosition))
+console.log('Fewer Steps taken for Intersection:' , fewStepTakenDistance(crossedPosition));
 
-    resetDirection(direction);
-    mapWires(wirePath2, directionSetWire2, false);
 
-    findIntersectionPoints(directionSetWire1, directionSetWire2);
-    console.log('Distance: ' ,   findDistance(crossedPosition))
-}
-
-// findDistanceOfClosestIntersectionPoint();
-
-findShortestStepIntersectionPoint();
 
 
 
--- script/dec4.js
+++ script/dec4.js
@@ -1,8 +1,68 @@
-const PUZZLE_INPUT = 153517-630395;
-let range = {
-    start: 153517,
-    end: 630395
+// const PUZZLE_INPUT = 153517-630395;
+// let range = {
+//     start: 153517,
+//     end: 630395
+// }
+
+
+// const checkPassword = function(passwordRange){
+//     let totalPossiblePassword = 0
+//     for(let currentPassword = passwordRange.start; currentPassword<=passwordRange.end; currentPassword++){
+//         let password = (currentPassword  + '') .split('');
+//         let isAdjacentSameNumber = hasAdjacentSameNumbers(password);
+//         let isIncreasingNumbers = hasIncreasingNumbers(password);
+        
+//         if(isIncreasingNumbers && isAdjacentSameNumber){
+//             totalPossiblePassword++; 
+//         }
+//     }
+//     return totalPossiblePassword;
+// }
+
+// const hasAdjacentSameNumbers = function (password) {
+//     let isAdjacentSameNumber = false;
+//     password.map((digit, index)  => {
+//         if(password[index] == password[index+1])
+//             isAdjacentSameNumber = true;
+//     })
+    
+//     return isAdjacentSameNumber;
+// }
+
+// const hasIncreasingNumbers = function (password){
+//     let isIncreasingNumbers = true;
+//     password.map((digit, index) => {
+//         if(digit > password[index+1] && isIncreasingNumbers)
+//             isIncreasingNumbers = false
+//     })
+
+//     return isIncreasingNumbers;
+// }
+// function hasGroupOfOnlyTwo(pwd) { // For part 2
+//     return (pwd.match(/(\d)\1+/g) || []).map(sequence => sequence.length).includes(2);
+//   }
+
+// console.log(checkPassword(range))
+
+const start = 153517;
+const end = 647015;
+
+function isMonotone(pwd) {
+  return pwd.split('').every((digit, index) => index === 0 || digit >= pwd[index - 1]);
+}
+function hasGroupOfTwo(pwd) { // For part 1
+  return /(\d)\1/.test(pwd);
+}
+function hasGroupOfOnlyTwo(pwd) { // For part 2
+  return (pwd.match(/(\d)\1+/g) || []).map(sequence => sequence.length).includes(2);
+}
+function isCandidate(pwd) {
+  return hasGroupOfOnlyTwo(pwd) && isMonotone(pwd);
 }
 
+let validCount = 0;
+for (let pwd = start; pwd <= end; pwd++) {
+  validCount += isCandidate(String(pwd));
+}
 
-
+console.log(validCount);
