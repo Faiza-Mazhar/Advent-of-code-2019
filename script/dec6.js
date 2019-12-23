0f51bc899d72c7544863bb7da3bb0d63f818856d
Faiza Mazhar <faiza.mazhar@bbc.co.uk>
2019-12-23 11:32:03 +0000

    Calculating total no. of direct and indirect orbits for given example

--- script/dec6.js
+++ script/dec6.js
@@ -1,30 +1,53 @@
 const fs = require('fs');
-// let orbitInfo = fs.readFileSync('../input/dec6', 'utf8').split('\n').map(value => value.split(')'));
- let orbitInfo = "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L".split(' ').map(value => value.split(')'));
- console.log(orbitInfo)
+let orbitInfo = fs.readFileSync('../input/dec6', 'utf8').split('\n').map(value => value.split(')'));
+//   let orbitInfo = "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L 9)3".split(' ').map(value => value.split(')'));
+//  console.log(orbitInfo)
 
 const createOrbitStructure = () => {
     let orbitInfoPerMoon = [ [orbitInfo[0][0]] ];
     orbitInfo.forEach(orbit => {
+        let orbitHandled = false;
         orbitInfoPerMoon.forEach( currentMoonInfo => {
-            if(currentMoonInfo.includes(orbit[0])){
-                if(currentMoonInfo.indexOf(orbit[0]) == currentMoonInfo.length-1){
+            if(currentMoonInfo.includes(orbit[0]) && currentMoonInfo.indexOf(orbit[0]) == currentMoonInfo.length-1){
                     currentMoonInfo.push(orbit[1]);
-                } else {
-                orbitInfoPerMoon.push(orbit);
-            }
+                    orbitHandled = true;
+                } 
+        });
+
+        if(!orbitHandled){
+            orbitInfoPerMoon.push(orbit);
         }
-    });
 });
-    console.log(orbitInfoPerMoon)
+    return orbitInfoPerMoon;
 }
 
+    const calculateOrbits = (orbitInfoPerMoons) =>{
+        let moonOrbitCount = {}
 
-// orbitInfo.forEach(function each(item) {
-//     if (Array.isArray(item))
-//       item.forEach(each);
-//     else
-//       console.log(item)
-//   });
+        orbitInfoPerMoons.map( orbitInfoMoon => {
+            let orbitCount;
+            let parentMoonOrbitInfo = 0;
+            orbitInfoMoon.map( (moon , index)  => {
+                orbitCount = index  + parentMoonOrbitInfo;
+                if(!moonOrbitCount.hasOwnProperty(moon)){
+                    moonOrbitCount[moon] = orbitCount;
+                } else{
+                    parentMoonOrbitInfo = moonOrbitCount[moon];
+                }
+            })
+        })
 
-  createOrbitStructure()
+        return moonOrbitCount;
+    }
+
+    const calculateTotalOrbits = (moonOrbitCount) => {
+        let totalNoOfOrbits = 0;
+        for( moon in moonOrbitCount){
+            totalNoOfOrbits += moonOrbitCount[moon];
+        }
+        return totalNoOfOrbits;
+    }
+
+
+
+  console.log(calculateTotalOrbits ( calculateOrbits( createOrbitStructure() ) ) )
