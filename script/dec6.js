4fa3f11362129e933b00863a405765d9e74a2708
Faiza Mazhar <faiza.mazhar@bbc.co.uk>
2019-12-23 13:34:21 +0000

    Use Objects and reduce, reworked the logic, part 1 working now

--- script/dec6.js
+++ script/dec6.js
@@ -1,53 +1,18 @@
 const fs = require('fs');
-let orbitInfo = fs.readFileSync('../input/dec6', 'utf8').split('\n').map(value => value.split(')'));
-//   let orbitInfo = "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L 9)3".split(' ').map(value => value.split(')'));
-//  console.log(orbitInfo)
 
-const createOrbitStructure = () => {
-    let orbitInfoPerMoon = [ [orbitInfo[0][0]] ];
-    orbitInfo.forEach(orbit => {
-        let orbitHandled = false;
-        orbitInfoPerMoon.forEach( currentMoonInfo => {
-            if(currentMoonInfo.includes(orbit[0]) && currentMoonInfo.indexOf(orbit[0]) == currentMoonInfo.length-1){
-                    currentMoonInfo.push(orbit[1]);
-                    orbitHandled = true;
-                } 
-        });
+let orbitInfo = fs.readFileSync('../input/dec6', 'utf8');
 
-        if(!orbitHandled){
-            orbitInfoPerMoon.push(orbit);
-        }
-});
-    return orbitInfoPerMoon;
+const orbitMap = orbitInfo.split('\n').reduce( (mapObject, currentOrbitInfo) => {
+  currentOrbitInfo = currentOrbitInfo.split(')');
+  mapObject[currentOrbitInfo[1]] = currentOrbitInfo[0];
+  return mapObject;
+}, {}); 
+
+
+const countParents = (child) => child in orbitMap ? 1 + countParents(orbitMap[child]) : 0;
+
+const totalNoOfDirectIndirectOrbits = (orbitMap) => {
+  return Object.keys(orbitMap).reduce( (cumulativeSum, child) => cumulativeSum + countParents(child), 0)
 }
 
-    const calculateOrbits = (orbitInfoPerMoons) =>{
-        let moonOrbitCount = {}
-
-        orbitInfoPerMoons.map( orbitInfoMoon => {
-            let orbitCount;
-            let parentMoonOrbitInfo = 0;
-            orbitInfoMoon.map( (moon , index)  => {
-                orbitCount = index  + parentMoonOrbitInfo;
-                if(!moonOrbitCount.hasOwnProperty(moon)){
-                    moonOrbitCount[moon] = orbitCount;
-                } else{
-                    parentMoonOrbitInfo = moonOrbitCount[moon];
-                }
-            })
-        })
-
-        return moonOrbitCount;
-    }
-
-    const calculateTotalOrbits = (moonOrbitCount) => {
-        let totalNoOfOrbits = 0;
-        for( moon in moonOrbitCount){
-            totalNoOfOrbits += moonOrbitCount[moon];
-        }
-        return totalNoOfOrbits;
-    }
-
-
-
-  console.log(calculateTotalOrbits ( calculateOrbits( createOrbitStructure() ) ) )
+console.log(totalNoOfDirectIndirectOrbits(orbitMap));
